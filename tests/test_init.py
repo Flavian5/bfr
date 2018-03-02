e049fc4b52db14a0df16e0695084a3ea13d9c533
JB <jeppeb91@hotmail.com>
2018-03-03 00:53:32 +0100

    Rewrote docstring

--- tests/test_init.py
+++ tests/test_init.py
@@ -7,12 +7,12 @@ class BasicTests(unittest.TestCase):
     """Basic test cases."""
 
     def test_init_cluster(self):
-        """
+        """ Passed if a cluster is initialised with the correct attributes and dimensionality
 
-        Passed if a cluster is initialised with the correct attributes and dimensionality
         -------
 
         """
+
         dimensions = 10
         cluster = bfr.init_cluster(dimensions)
         self.assertEqual(len(cluster), 2, "Cluster has incorrect number of attributes")
